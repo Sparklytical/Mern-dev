3217f9d8118a30006876ec3d0fb6a9bf62eca6eb
EvilSparkm <kamrantahir25@gmail.com>
2019-12-25 23:41:28 +0530

    :sparkles: Models
    Models for users, profile and posts

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+
+<a name="1.0.0"></a>
+## 1.0.0 (2019-12-25)
+
+### Added
+
+- ✨ mongoURI [[da14e80](https://github.com/EvilSpark/Mern-dev/commit/da14e808a3a78cbc18e54270aabf06843d9a25e4)]
+
+### Changed
+
+- 🎨 Routes [[7426cbe](https://github.com/EvilSpark/Mern-dev/commit/7426cbe5568732ec201978e48e221b3fd6c609e4)]
+
+### Miscellaneous
+
+-  Initial commit [[36573ef](https://github.com/EvilSpark/Mern-dev/commit/36573efc64afd93ce8df496e2d894de91d85d13a)]
+
+
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,29 @@
 const express = require("express");
 
 const router = express.Router();
+// Load user model
+
+const User = require("../../models/Users");
+
 //@route GET api/users/test
 
 router.get("/test", (req, res) => res.json({ msg: "user works" }));
+
+// @route GET api/users/register
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
+  });
+});
 
 module.exports = router;
